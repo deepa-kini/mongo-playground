e06c8769503d6472c8c25a9d82da58b6024c72b8
deepa-kini-lux <deepa.kini@luxeveryday.com>
2022-02-15 16:06:30 +1100

    Update directly using updateOne method

--- index.js
+++ index.js
@@ -48,16 +48,22 @@ const Course = mongoose.model('Course', courseSchema);
 // getCourses();
 
 async function updateCourse(id) {
-  const course = await Course.findById(id);
+  // const course = await Course.findById(id);
+  const course = await Course.updateOne({
+    _id: id
+  }, {
+    isPublished: true,
+    author: 'Self again'
+  });
   if (!course) return;
-  course.isPublished = true;
-  course.author = 'Another';
+  // course.isPublished = true;
+  // course.author = 'Another';
   // course.set({
   //   isPublished: true,
   //   author: 'Another author'
   // });
-  const result = await course.save();
-  console.log('Updated:', result)
+  // const result = await course.save();
+  console.log('Updated:', course)
 }
 
 updateCourse('620b13123bcf1aba2cc7faa1');
