1334fef3d33ea9388d7cd80a13a56fee4707b632
deepa-kini-lux <deepa.kini@luxeveryday.com>
2022-02-15 13:53:12 +1100

    Create a document in Mongo DB

--- index.js
+++ index.js
@@ -7,3 +7,27 @@ const dbConnect = `mongodb+srv://root:${password}@playground.mryia.mongodb.net/p
 mongoose.connect(dbConnect)
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.log('error', err.message));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
+const Course = mongoose.model('Course', courseSchema);
+
+
+async function createCourse() {
+  const course = new Course({
+    name: 'My MongoDB Examples',
+    author: 'Self',
+    tags: ['mongo', 'database', 'backend'],
+    isPublished: true
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+createCourse();
