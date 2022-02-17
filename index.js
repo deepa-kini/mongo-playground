a92ca9a5c098deefd9c98c89286d7bdde66fe1f9
deepa-kini-lux <deepa.kini@luxeveryday.com>
2022-02-17 14:32:11 +1100

    Custom validators

--- index.js
+++ index.js
@@ -9,27 +9,52 @@ mongoose.connect(dbConnect)
   .catch(err => console.log('error', err.message));
 
 const courseSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ['web', 'mobile', 'network']
+  },
   author: String,
-  tags: [String],
+  tags: {
+    type: [String],
+    validate: {
+      validator: function (v) {
+        return v && v.length > 0;
+      },
+      message: 'A course should have at least one tag'
+    }
+  },
   date: { type: Date, default: Date.now },
-  isPublished: Boolean
+  isPublished: Boolean,
+  price: {
+    type: Number,
+    required: function () { return this.isPublished; },
+    min: 10,
+    max: 200
+  }
 });
 const Course = mongoose.model('Course', courseSchema);
 
 
 async function createCourse() {
   const course = new Course({
-    // name: 'My MongoDB Examples',
+    name: 'My MongoDB Examples',
+    category: 'web',
     author: 'Self',
     tags: ['mongo', 'database', 'backend'],
-    isPublished: true
+    isPublished: true,
+    price: 15
   });
 
   try {
-    await course.validate()
-    // const result = await course.save();
-    // console.log(result);
+    const result = await course.save();
+    console.log(result);
   } catch (ex) {
     console.log(ex.message);
   }
